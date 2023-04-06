ec7c8dd26210c6906f900c764249543a4cb33d47
Olga Levchenko <olya.kovalchenko@gmail.com>
2023-04-06 09:53:46 +0200

    feat: create button

---
+++ src/Button/Button.js
@@ -0,0 +1,4 @@
+const myStyle = { color: '#ffffff', background: '#15485A', border: 'none', padding:'10px 30px', borderRadius:'100px'};
+export const Button = () => {
+    return <div><button style={myStyle}>Button</button></div>
+   };
