8712ff24c11b43d74a456db6b35902f47448ba4b
Olga Levchenko <olya.kovalchenko@gmail.com>
2023-04-06 15:58:57 +0200

    feat: create Button.module.css

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,14 +1,15 @@
-const myStyle = {
-  color: '#ffffff',
-  background: '#15485A',
-  border: 'none',
-  padding: '10px 30px',
-  borderRadius: '100px',
-};
+// const myStyle = {
+//   color: '#ffffff',
+//   background: '#15485A',
+//   border: 'none',
+//   padding: '10px 30px',
+//   borderRadius: '100px',
+// };
+import styles from './Button.module.css';
 export const Button = () => {
   return (
     <div>
-      <button style={myStyle}>Button</button>
+      <button className={styles.myStyle}>Button</button>
     </div>
   );
 };
