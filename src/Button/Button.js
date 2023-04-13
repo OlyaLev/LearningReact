fcde8bd29ca8f1d49b3c33f00ba479111fa2e098
Olga Levchenko <olya.kovalchenko@gmail.com>
2023-04-13 15:45:24 +0200

    fix: change var's names

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -9,22 +9,22 @@
 import styles from './Button.module.css';
 export const Button = (props) => {
   const { children, fullWidth, variant } = props;
-  const isFullWidth = fullWidth ? styles.fullWidth : '';
-  const isOutlined = variant === 'outlined' ? styles.outlinedButton : '';
-  const isMainColors = variant === 'filled' ? '' : styles.mainColors;
-  const isFilled = variant === 'filled' ? styles.filledButton : '';
+  const fullWidthStyle = fullWidth ? styles.fullWidth : '';
+  const outlinedStyle = variant === 'outlined' ? styles.outlinedButton : '';
+  const mainColorsStyle = variant === 'filled' ? '' : styles.mainColors;
+  const filledStyle = variant === 'filled' ? styles.filledButton : '';
   return (
     <button
       className={
         styles.buttonCommon +
         ' ' +
-        isMainColors +
+        mainColorsStyle +
         ' ' +
-        isFullWidth +
+        fullWidthStyle +
         ' ' +
-        isFilled +
+        filledStyle +
         ' ' +
-        isOutlined
+        outlinedStyle
       }
     >
       {children}
