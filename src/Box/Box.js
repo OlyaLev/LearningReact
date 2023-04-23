1ad9444e20af7dea262941ed7dea9503ba6f0eb8
Olga Levchenko <olya.kovalchenko@gmail.com>
2023-04-23 17:22:45 +0200

    fix: change styles

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -8,10 +8,45 @@ const directions = {
 const wraps = {
   wrap: styles.wrap,
   wrapReverse: styles.wrapReverse,
+  noWrap: styles.noWrap,
 };
+
+const alignXVariants = {
+  alignXStart: styles.alignXStart,
+  alignXEnd: styles.alignXEnd,
+  alignXCenter: styles.alignXCenter,
+  alignXSpaceBetween: styles.alignXSpaceBetween,
+  alignXSpaceAround: styles.alignXSpaceAround,
+  alignXSpaceEvenly: styles.alignXSpaceEvenly,
+};
+
+const alignYVariants = {
+  alignYStart: styles.alignYStart,
+  alignYEnd: styles.alignYEnd,
+  alignYCenter: styles.alignYCenter,
+  alignYBaseline: styles.alignYBaseline,
+  alignYStretch: styles.alignYStretch,
+};
+
 export const Box = (props) => {
   const { direction, alignX, alignY, wrap } = props;
   const directionStyle = directions[direction];
+  const alignXStyle = alignXVariants[alignX];
+  const alignYStyle = alignYVariants[alignY];
   const wrapStyle = wraps[wrap];
-  return <div className={styles.boxCommon + ' ' + directionStyle + ' ' + wrapStyle}></div>;
+  return (
+    <div
+      className={
+        styles.boxCommon +
+        ' ' +
+        directionStyle +
+        ' ' +
+        alignXStyle +
+        ' ' +
+        alignYStyle +
+        ' ' +
+        wrapStyle
+      }
+    ></div>
+  );
 };
